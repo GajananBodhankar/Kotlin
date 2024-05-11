d2046d20ce61371a34f3ce23e3788fac25caf0ab
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-11 21:39:17 +0530

    Initialized kotlin project with tutorials

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-var num=0;
+var num=0
     for (i in 1..10 step 2){
         println("$i - "+(++num))
     }
