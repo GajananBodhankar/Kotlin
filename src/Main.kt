33946b660d3bbe3286fcdd2a919d6b644ef50726
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-11 21:39:58 +0530

    Initialized kotlin project with tutorials

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 var num=0
-    for (i in 1..10 step 2){
-        println("$i - "+(++num))
+    for (i in 1..10 step 2) {
+        println("$i - " + (++num))
     }
-    println()
 }
